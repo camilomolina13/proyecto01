6991ad50a646b4c48547c572fe4e42484063dc2c
Juan Camilo Molina Valencia <camilomolina321@gmail.com>
2021-03-11 13:40:34 -0500

    Agregando pagina2.php con algo de concatenación

---
+++ pagina2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang = "es">
+<head>
+    <title>Proyecto Heroku</title>
+    <meta charset = "UTF-8" />
+    <link rel = "stylesheet" content = "text/css" href = "css/estilos.css"/>
+    <link rel = "stylesheet" content = "text/css" href = "css/normalize.css"/>
+</head>
+    <body>
+        <?php 
+            $a = 12;
+            $b = 1000;
+
+            $a.= $b;
+
+            echo "<br /> Concatenando $a."
+        ?>
+    </body>      
+</html>
